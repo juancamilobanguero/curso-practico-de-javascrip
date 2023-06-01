db2d311c98cf4c88f75e34dfd35e9d56e5cf8fb3
juancamilobanguero <banguerojuancamilo58@gmail.com>
2023-06-01 11:31:59 -0500

    develop

--- ejercicios.js
+++ ejercicios.js
@@ -19,3 +19,20 @@ function tipoSuscripcion(suscripcion){
     console.warn('ese tipo de suscripcion no existe')
     
 }
+
+function moto(llantas,dirrecion,tanque){
+    this.llantas = llantas
+    this.dirrecion = dirrecion
+    this.tanque = tanque
+}
+let motos = []
+for(let i; i< 1; i++){
+    let llantas  = prompt("ingresa la llanta que desea")
+    let dirrecion = prompt("ingresa la dirrecion que desea")
+    let tanque = prompt("ingresa el tanque que desea")
+
+    motos.push(new motos(llantas,dirrecion,tanque))
+}
+for(let i; i <motos.length;i++){
+    console.log([i])
+}
